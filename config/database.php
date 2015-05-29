4ea096a9302eda52b375c12e6a71dd3a314a8d78
jhadvig <j.hadvig@gmail.com>
2015-05-29 15:30:37 +0200

    qwe

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
 	|
 	*/
 
-	'default' => env('DB_DRIVER', 'mysql'),
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -48,38 +48,18 @@ return [
 	*/
 
 	'connections' => [
-
-		'sqlite' => [
-			'driver'   => 'sqlite',
-			'database' => storage_path().'/database.sqlite3',
-			'prefix'   => '',
-		],
-
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', env('OPENSHIFT_MYSQL_DB_HOST', 'localhost')),
-			'port'      => env('DB_PORT', env('OPENSHIFT_MYSQL_DB_PORT', 3306)),
-			'database'  => env('DB_DATABASE', env('OPENSHIFT_APP_NAME', 'forge')),
-			'username'  => env('DB_USERNAME', env('OPENSHIFT_MYSQL_DB_USERNAME', 'forge')),
-			'password'  => env('DB_PASSWORD', env('OPENSHIFT_MYSQL_DB_PASSWORD', '')),
+			'host'      => env('DATABASE_SERVICE_HOST', '172.17.44.1'),
+			'port'      => env('DATABASE_SERVICE_PORT', 3306)),
+			'database'  => env('DATABASE_NAME', 'db')),
+			'username'  => env('DATABASE_USER', 'user')),
+			'password'  => env('DATABASE_PASSWORD', 'pass')),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
 			'strict'    => false,
-		],
-
-		'pgsql' => [
-			'driver'   => 'pgsql',
-			'host'     => env('DB_HOST', env('OPENSHIFT_POSTGRESQL_DB_HOST', 'localhost')),
-			'port'     => env('DB_PORT', env('OPENSHIFT_POSTGRESQL_DB_PORT', 5432)),
-			'database' => env('DB_DATABASE', env('OPENSHIFT_APP_NAME', 'forge')),
-			'username' => env('DB_USERNAME', env('OPENSHIFT_POSTGRESQL_DB_USERNAME', 'forge')),
-			'password' => env('DB_PASSWORD', env('OPENSHIFT_POSTGRESQL_DB_PASSWORD', '')),
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
 		]
-
 	],
 
 	/*
